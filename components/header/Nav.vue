856607b7fbda618ad22e8df2069a493cab4b05e0
yianliu <yianliu@outlook.com>
2023-01-18 19:25:44 +0000

    First commit

---
+++ components/header/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+	<div
+		class="relative bg-white flex p-6 items-center place-content-between shadow"
+	>
+		<span class="text-2xl mx-6" v-text="title"></span>
+		<nav>
+			<HeaderLink
+				v-for="page in pages"
+				:key="page.slug"
+				:destination="page.slug"
+				:name="page.name"
+			/>
+		</nav>
+	</div>
+</template>
+
+<script setup>
+const title = useAppConfig().title
+const pages = [
+	{
+		slug: "about",
+		name: "About"
+	},
+	{
+		slug: "home",
+		name: "home"
+	}
+]
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<HeaderNav />
+		<slot />
+	</div>
+</template>
