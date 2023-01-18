02490838cb78f65e2ab080151e94703e19d33ad7
yianliu <yianliu@outlook.com>
2023-01-18 19:32:26 +0000

    typechecking + readme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
