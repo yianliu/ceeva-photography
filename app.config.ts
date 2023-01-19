e53f58e30d10b622d5766e340a3f5802f1b3511a
Yian Liu <yianliu@yians-air.home>
2023-01-19 22:09:53 +0000

    Endpoint url

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,5 @@
 export default defineAppConfig({
-  title: 'Ceeva Liu Photography'
+	title: "Ceeva Liu Photography",
+	hygraphUrl:
+		"https://eu-west-2.cdn.hygraph.com/content/cld2176280l5u01un6l4beiq8/master"
 })
