e56bd7bb4961815f19a00db95b1e3b24ac35e425
yianliu <yianliu@outlook.com>
2023-02-19 18:59:16 +0000

    Fixed hook

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,8 @@
 <script setup>
 	const { slug } = useRoute().params
 
-	const story = await useAsyncStoryblok(
-		slug && slug.length > 0 ? slug.join("/") : "home",
-		{ version: "draft" }
+	const story = await useStory(
+		slug && slug.length > 0 ? slug.join("/") : "home"
 	)
 </script>
 
