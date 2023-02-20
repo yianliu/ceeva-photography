619fbb9990b9c8ac54ab96dcbcab6aaef9a5eb10
yianliu <yianliu@outlook.com>
2023-02-20 20:58:57 +0000

    Removed image resizing

--- storyblok/Gallery.vue
+++ storyblok/Gallery.vue
@@ -4,11 +4,14 @@
 			v-text="blok.title"
 			class="text-3xl text-center my-12"></h1>
 		<div class="columns-1 md:columns-2 lg:columns-3 gap-0">
-			<nuxt-img
+			<div
 				v-for="image in blok.images"
-				:key="image.id"
-				:src="`${image.filename}/m/`"
-				loading="lazy" />
+				:key="image.id">
+				<img
+					:src="image.filename"
+					loading="lazy" />
+				<div v-text="image.title"></div>
+			</div>
 		</div>
 	</div>
 </template>
