e3cc90a47d23db90603d44233eb19b2d1c269155
yianliu <yianliu@outlook.com>
2023-02-22 17:22:19 +0000

    banner

---
+++ storyblok/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+	<div
+		v-editable="blok"
+		class="bg-cover bg-fixed p-12"
+		:style="blok.background?.filename && { backgroundImage }">
+		<h1>{{ blok.heading }}</h1>
+		{{ blok }}
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { BannerStoryblok } from "./component-types-sb"
+
+	const { blok } = defineProps<{ blok: BannerStoryblok }>()
+	const backgroundImage = `url(${
+		blok.mobile_background?.filename ||
+		blok.tablet_background?.filename ||
+		blok.background?.filename
+	})`
+</script>
