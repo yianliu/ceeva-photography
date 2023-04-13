39b015320d656472566232f7ab2e7f0ab39f772a
yianliu <yianliu@outlook.com>
2023-04-13 21:28:56 +0100

    head

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
 		<NuxtPage />
 	</NuxtLayout>
 </template>
+<script setup lang="ts">
+	useHead({
+		title: "Ceeva Liu Photography | Manchester",
+		meta: [
+			{
+				name: "description",
+				content: "Freelance photographer based in Manchester"
+			}
+		],
+		htmlAttrs: { lang: "en" }
+	})
+</script>
