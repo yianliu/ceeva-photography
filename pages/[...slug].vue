8e30744575a7b56ace93c6ce49885c383047f57e
yianliu <yianliu@outlook.com>
2023-04-13 22:07:02 +0100

    home redirect

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,8 @@
 <script setup>
 	const { slug } = useRoute().params
+	if (slug == "home") {
+		await navigateTo("/")
+	}
 	const environment = useRuntimeConfig().public.environment
 	const story = await useStory(
 		slug && slug.length > 0 ? slug.join("/").replace(/\/$/, "") : "home",
