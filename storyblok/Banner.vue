de6275690af54e4e1098aae2d03d11762af11fdb
yianliu <yianliu@outlook.com>
2023-09-10 10:54:33 +0100

    banner bg

--- storyblok/Banner.vue
+++ storyblok/Banner.vue
@@ -1,7 +1,7 @@
 <template>
 	<div
 		v-editable="blok"
-		class="flex flex-col bg-no-repeat bg-cover md:bg-fixed py-24 px-8 sm:p-8 md:p-12 lg:p-16 blok sm:min-h-screen justify-center"
+		class="flex flex-col bg-no-repeat bg-cover bg-center md:bg-fixed py-24 px-8 sm:p-8 md:p-12 lg:p-16 blok min-h-screen justify-center"
 		:class="[
 			`items-${blok.position}`,
 			blok.alignment && alignmentClasses[blok.alignment].text
