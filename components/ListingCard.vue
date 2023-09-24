32b2923c6ac8514e72467001752a5c504e71f255
yianliu <yianliu@outlook.com>
2023-09-24 19:44:59 +0100

    listing card height

--- components/ListingCard.vue
+++ components/ListingCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="text-gray-800 shadow-lg border border-solid border-gray-200 rounded-md p-6">
+		class="text-gray-800 h-full shadow-lg border border-solid border-gray-200 rounded-md p-6">
 		<div
 			v-text="title"
 			class="text-center lg:text-lg py-2 lg:py-8"></div>
